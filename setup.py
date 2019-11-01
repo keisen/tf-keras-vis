f0a73ebe67c3671120991a0c0b67a692e481c185
Keisen <k.keisen@gmail.com>
2019-11-01 15:25:50 +0900

    Update setup.py
    because of improving the verson param in that file.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="tf-keras-vis",
-    version="0.1.0",
+    version="0.1.0-dev",
     author="Keisen",
     author_email="k.keisen@gmail.com",
     description="Neural network visualization toolkit for tf.keras",
