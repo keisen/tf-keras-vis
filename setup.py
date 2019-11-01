3db7931ab0213d11b85b22781b35543ee40aa126
keisen <k.keisen@gmail.com>
2019-11-01 15:19:09 +0900

    Define requirements in setup.py and configure in setup.cfg

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
-import setuptools
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="tf-keras-vis",
-    version="0.0.1",
+    version="0.1.0",
     author="Keisen",
     author_email="k.keisen@gmail.com",
     description="Neural network visualization toolkit for tf.keras",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/keisen/tf-keras-vis",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=['scipy', 'six', 'matplotlib'],
+    extras_require={
+      'cpu': ['tensorflow >= 2.0'],
+      'gpu': ['tensorflow-gpu >= 2.0'],
+      'dev': ['flake8', 'isort==4.3.*', 'yapf==0.28.*'],
+      'vis': ['Pillow', 'imageio'],
+      'tests': ['pytest', 'pytest-pep8', 'pytest-xdist', 'pytest-cov'],
+      'examples': ['jupyterlab'],
+    },
+    include_package_data=True,
 )
