8b8a7eb6898ac56f6304d8940987534610faf414
keisen <k.keisen@gmail.com>
2020-05-07 17:50:51 +0900

    Add supporting Python3.8+

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5, <3.8',
+    python_requires='>=3.5, <=3.8',
     install_requires=['numpy', 'scipy', 'imageio', 'pillow'],
     extras_require={
         'development':
