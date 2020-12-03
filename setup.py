9b3d509a5c45d3c354ed650378939589841be41b
keisen <k.keisen@gmail.com>
2020-12-03 14:10:04 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="tf-keras-vis",
-    version="0.5.5",
+    version="0.6.0",
     author="keisen",
     author_email="k.keisen@gmail.com",
     description="Neural network visualization toolkit for tf.keras",
@@ -19,9 +19,9 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6, <3.9',
-    install_requires=['numpy', 'scipy', 'imageio', 'pillow'],
+    install_requires=['scipy==1.4.*', 'pillow==7.1.*', 'deprecated'],
     extras_require={
-        'develop': ['flake8', 'isort', 'yapf', 'pytest<6.0', 'pytest-pep8', 'pytest-cov'],
+        'develop': ['flake8', 'isort', 'yapf', 'pytest', 'pytest-pep8', 'pytest-cov'],
         'examples': ['jupyterlab', 'matplotlib'],
     },
     include_package_data=True,
