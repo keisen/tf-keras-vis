425e9ad4c0c53589f92462bdf516b4588e7cf45f
keisen <k.keisen@gmail.com>
2020-12-15 10:57:34 +0000

    Prepare to release 0.5.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="tf-keras-vis",
-    version="0.5.4",
+    version="0.5.5",
     author="keisen",
     author_email="k.keisen@gmail.com",
     description="Neural network visualization toolkit for tf.keras",
