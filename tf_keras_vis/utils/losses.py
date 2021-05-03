a9444ae0ef7feb3f3f44122a40cdeddc822e952e
keisen <k.keisen@gmail.com>
2021-05-03 14:19:19 +0900

    Remove some dead-codes

--- tf_keras_vis/utils/losses.py
+++ tf_keras_vis/utils/losses.py
@@ -1,9 +1,14 @@
 import warnings
+
 warnings.warn(('`tf_keras_vis.utils.losses` module is deprecated. '
                'Please use `tf_keras_vis.utils.scores` instead.'), DeprecationWarning)
 
-from tf_keras_vis.utils.scores import BinaryScore as BinaryLoss  # noqa: F401 E402
-from tf_keras_vis.utils.scores import CategoricalScore as CategoricalLoss  # noqa: F401 E402
-from tf_keras_vis.utils.scores import InactiveScore as InactiveLoss  # noqa: F401 E402
+from tf_keras_vis.utils.scores import \
+    BinaryScore as BinaryLoss  # noqa: F401 E402
+from tf_keras_vis.utils.scores import \
+    CategoricalScore as CategoricalLoss  # noqa: F401 E402
+from tf_keras_vis.utils.scores import \
+    InactiveScore as InactiveLoss  # noqa: F401 E402
 from tf_keras_vis.utils.scores import Score as Loss  # noqa: F401 E402
-from tf_keras_vis.utils.scores import SmoothedCategoricalScore as SmoothedCategoricalLoss  # noqa: F401 E402 E501
+from tf_keras_vis.utils.scores import \
+    SmoothedCategoricalScore as SmoothedCategoricalLoss  # noqa: F401 E402 E501
