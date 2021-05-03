Fixes a wrong of mean calculation

tf_keras_vis/scorecam.py
@@ -4,8 +4,7 @@ import tensorflow.keras.backend as K
 from scipy.ndimage.interpolation import zoom
 
 from tf_keras_vis.gradcam import Gradcam
-from tf_keras_vis.utils import (is_mixed_precision, listify, standardize,
-                                zoom_factor)
+from tf_keras_vis.utils import (is_mixed_precision, listify, standardize, zoom_factor)
 
 
 class Scorecam(Gradcam):
@@ -132,8 +131,7 @@ class Scorecam(Gradcam):
         # Calculating weights
         weights = ([score(p) for p in prediction] for score, prediction in zip(scores, preds))
         weights = (np.array(w, dtype=np.float32) for w in weights)
-        weights = (np.mean(w, axis=tuple(range(len(w.shape)))[1:]) if len(w.shape) > 2 else w
-                   for w in weights)
+        weights = (np.mean(w, axis=tuple(range(len(w.shape)))[1:]) for w in weights)
         weights = (np.reshape(w, (channels, -1)) for w in weights)
         weights = np.array(list(weights), dtype=np.float32)
         weights = np.sum(weights, axis=0)