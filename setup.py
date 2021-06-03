e2e300aacba51558cf65133c8ed684ccbd760a3c
keisen <k.keisen@gmail.com>
2021-06-04 00:32:40 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="tf-keras-vis",
-    version="0.6.2",
+    version="0.6.3",
     author="keisen",
     author_email="k.keisen@gmail.com",
     description="Neural network visualization toolkit for tf.keras",
