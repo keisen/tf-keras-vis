Fixes a bug that the result of calculation in BinaryScore is wrong. And improve typing hints.

tests/tf_keras_vis/activation_maximization/activation_maximization_test.py
@@ -11,7 +11,7 @@ from tf_keras_vis.activation_maximization.regularizers import Norm, TotalVariati
 from tf_keras_vis.utils.regularizers import LegacyRegularizer
 from tf_keras_vis.utils.regularizers import Norm as LegacyNorm
 from tf_keras_vis.utils.regularizers import TotalVariation2D as LegacyTotalVariation2D
-from tf_keras_vis.utils.scores import CategoricalScore
+from tf_keras_vis.utils.scores import BinaryScore, CategoricalScore
 from tf_keras_vis.utils.test import (MockLegacyCallback, NO_ERROR, MockCallback, assert_error,
                                      dummy_sample, mock_conv_model,
                                      mock_conv_model_with_float32_output, mock_multiple_io_model,
@@ -485,7 +485,7 @@ class TestActivationMaximizationWithMultipleOutputsModel():
         ([CategoricalScore(0), None], ValueError),
         ([CategoricalScore(0), score_with_tuple], NO_ERROR),
         ([CategoricalScore(0), score_with_list], NO_ERROR),
-        ([CategoricalScore(0), CategoricalScore(0)], NO_ERROR),
+        ([CategoricalScore(0), BinaryScore(False)], NO_ERROR),
     ])
     def test__call__if_score_is_(self, scores, expected_error, multiple_outputs_model):
         activation_maximization = ActivationMaximization(multiple_outputs_model)
@@ -505,7 +505,7 @@ class TestActivationMaximizationWithMultipleOutputsModel():
     def test__call__if_seed_input_is_(self, seed_input, expected, multiple_outputs_model):
         activation_maximization = ActivationMaximization(multiple_outputs_model)
         result = activation_maximization(
-            [CategoricalScore(1), CategoricalScore(0)], seed_input=seed_input)
+            [CategoricalScore(1), BinaryScore(False)], seed_input=seed_input)
         if type(expected) is list:
             assert type(result) == list
             result = result[0]
@@ -526,7 +526,7 @@ class TestActivationMaximizationWithMultipleIOModel():
         ([CategoricalScore(0), None], ValueError),
         ([CategoricalScore(0), score_with_tuple], NO_ERROR),
         ([CategoricalScore(0), score_with_list], NO_ERROR),
-        ([CategoricalScore(0), CategoricalScore(0)], NO_ERROR),
+        ([CategoricalScore(0), BinaryScore(False)], NO_ERROR),
     ])
     def test__call__if_score_is_(self, scores, expected_error, multiple_io_model):
         activation_maximization = ActivationMaximization(multiple_io_model)
@@ -551,7 +551,7 @@ class TestActivationMaximizationWithMultipleIOModel():
         activation_maximization = ActivationMaximization(multiple_io_model)
         with assert_error(expected_error):
             result = activation_maximization(
-                [CategoricalScore(1), CategoricalScore(0)], seed_input=seed_inputs)
+                [CategoricalScore(1), BinaryScore(True)], seed_input=seed_inputs)
             if seed_inputs is not None and seed_inputs[0].shape[0] == 4:
                 assert result[0].shape == (4, 8, 8, 3)
                 assert result[1].shape == (4, 10, 10, 3)
@@ -601,7 +601,7 @@ class TestActivationMaximizationWithMultipleIOModel():
         activation_maximization = ActivationMaximization(multiple_io_model)
         with assert_error(expected_error):
             result = activation_maximization(
-                [CategoricalScore(1), CategoricalScore(0)], input_modifiers=input_modifiers)
+                [CategoricalScore(1), BinaryScore(True)], input_modifiers=input_modifiers)
             assert result[0].shape == (1, 8, 8, 3)
             assert result[1].shape == (1, 10, 10, 3)
 
@@ -640,7 +640,7 @@ class TestActivationMaximizationWithMultipleIOModel():
         activation_maximization = ActivationMaximization(multiple_io_model)
         with assert_error(expected_error):
             result = activation_maximization(
-                [CategoricalScore(0), CategoricalScore(0)], regularizers=regularizers)
+                [CategoricalScore(0), BinaryScore(True)], regularizers=regularizers)
             assert result[0].shape == (1, 8, 8, 3)
             assert result[1].shape == (1, 10, 10, 3)
 
@@ -698,7 +698,7 @@ class TestActivationMaximizationWithMultipleIOModel():
         activation_maximization = ActivationMaximization(multiple_io_model)
         with assert_error(expected_error):
             result = activation_maximization(
-                [CategoricalScore(0), CategoricalScore(0)], regularizers=regularizers)
+                [CategoricalScore(0), BinaryScore(True)], regularizers=regularizers)
             assert result[0].shape == (1, 8, 8, 3)
             assert result[1].shape == (1, 10, 10, 3)
 
@@ -721,11 +721,11 @@ class TestActivationMaximizationWithMultipleIOModel():
         activation_maximization = ActivationMaximization(multiple_io_model)
         with assert_error(expected_error):
             result = activation_maximization(
-                [CategoricalScore(1), CategoricalScore(0)], seed_input=seed_inputs)
+                [CategoricalScore(1), BinaryScore(True)], seed_input=seed_inputs)
             assert not np.all(result[0] == 0.0)
             assert not np.all(result[1] == 0.0)
             result = activation_maximization(
-                [CategoricalScore(1), CategoricalScore(0)],
+                [CategoricalScore(1), BinaryScore(True)],
                 seed_input=seed_inputs,
                 activation_modifiers=activation_modifiers)
             if modified_0:
@@ -844,7 +844,7 @@ class TestMixedPrecision():
 
     def _test_for_multiple_io(self, model):
         activation_maximization = ActivationMaximization(model)
-        result = activation_maximization([CategoricalScore(1), CategoricalScore(0)])
+        result = activation_maximization([CategoricalScore(1), BinaryScore(False)])
         assert result[0].shape == (1, 8, 8, 3)
         assert result[1].shape == (1, 10, 10, 3)
 

tests/tf_keras_vis/utils/scores_test.py
@@ -54,7 +54,7 @@ class TestBinaryScore():
         score = BinaryScore(target_values)
         with assert_error(expected_error):
             score_value = score(output)
-            assert score_value == expected
+            assert tf.math.reduce_all(score_value == expected)
 
 
 class TestCategoricalScore():

tf_keras_vis/utils/scores.py
@@ -28,7 +28,8 @@ class Score(ABC):
             NotImplementedError: This method must be overwritten.
 
         Returns:
-            Union[tf.Tensor, list, tuple]: Score values or a list or tuple of them.
+            Union[tf.Tensor, list[tf.Tensor], tuple[tf.Tensor]]:
+                Score values or a list or tuple of them.
         """
         raise NotImplementedError()
 
@@ -42,7 +43,7 @@ class InactiveScore(Score):
     Todo:
         * Write examples
     """
-    def __init__(self):
+    def __init__(self) -> None:
         """Constructor.
         """
         super().__init__('InactiveScore')
@@ -58,7 +59,7 @@ class BinaryScore(Score):
     Todo:
         * Write examples
     """
-    def __init__(self, target_values):
+    def __init__(self, target_values) -> None:
         """Constructor.
 
         Args:
@@ -76,15 +77,14 @@ class BinaryScore(Score):
         if len(self.target_values) == 0:
             raise ValueError(f"target_values is required. target_values: {target_values}")
 
-    def __call__(self, output) -> list:
-        if output.ndim != 1 and not (output.ndim == 2 and output.shape[1] == 1):
+    def __call__(self, output) -> tf.Tensor:
+        if not (output.ndim == 2 and output.shape[1] == 1):
             raise ValueError(f"`output` shape must be (batch_size, 1), but was {output.shape}")
         output = tf.reshape(output, (-1, ))
         target_values = self.target_values
         if len(target_values) == 1 and len(target_values) < output.shape[0]:
             target_values = target_values * output.shape[0]
-        score = [val if positive else 1.0 - val for val, positive in zip(output, target_values)]
-        return score
+        return tf.math.abs(output - (1.0 - tf.constant(target_values, dtype=output.dtype)))
 
 
 class CategoricalScore(Score):
@@ -94,7 +94,7 @@ class CategoricalScore(Score):
     Todo:
         * Write examples
     """
-    def __init__(self, indices):
+    def __init__(self, indices) -> None:
         """Constructor.
 
         Args:
@@ -110,7 +110,7 @@ class CategoricalScore(Score):
         if len(self.indices) == 0:
             raise ValueError(f"`indices` is required. indices: {indices}")
 
-    def __call__(self, output):
+    def __call__(self, output) -> tf.Tensor:
         if output.ndim < 2:
             raise ValueError("`output` ndim must be 2 or more (batch_size, ..., channels), "
                              f"but was {output.ndim}")